24bfd9a2966f0a8d2d68034746d8f9566f0d172a
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2025-08-21 14:10:32 +0530

    Add Header and Footer files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Footer from "@/components/Footer";
+import  Header from "../components/Header";
 
 export default function Home() {
   return (
    <>
-    <div className=" text-4xl  text-amber-400 text-center"> Ha mai pagal hu</div>
+    <Header/>
+    <Footer/>
    </>
   );
 }
